0955166d7e4b41b0849e9ee55862114b6d570457
Marvin Altemeier <marvin.altemeier@hey.com>
2021-09-11 19:13:31 +0200

    Implement backwards moving

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -15,36 +15,42 @@ func _ready():
     $PickupSprite.hide()
 
 func _input(event):
-    if event.is_action_released("ui_interact") && interactable_object != null:
-        interactable_object.interact(self)
+	if event.is_action_released("ui_interact") && interactable_object != null:
+		interactable_object.interact(self)
 
-func handle_input():
-    rotation_direction = 0
-    velocity = Vector2.ZERO
+func handle_input(delta):
+	rotation_direction = 0
+	velocity = Vector2.ZERO
 
-    if Input.is_action_pressed("ui_right"):
-        rotation_direction += 1
-    if Input.is_action_pressed("ui_left"):
-        rotation_direction -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity -= transform.x * SPEED
-    if Input.is_action_pressed("ui_up"):
-        velocity += transform.x * SPEED
+	if Input.is_action_pressed("ui_right"):
+		rotation_direction += 1
+	if Input.is_action_pressed("ui_left"):
+		rotation_direction -= 1
+	if Input.is_action_pressed("ui_down"):
+		move_back()
+		return
+	if Input.is_action_pressed("ui_up"):
+		velocity += transform.x * SPEED
+
+	rotation += rotation_direction * ROTATION_SPEED * delta
+	velocity = move_and_slide(velocity)
+
+    # "Animate" the picked up object by slightly rotating it every tick
+	if pickup_object:
+		$PickupSprite.rotation += (2 * delta)
+
+	$PlayerTrail.paint_point()
+
+
+func move_back():
+	position = $PlayerTrail.pop_last()
 
 func _physics_process(delta):
-    handle_input()
-    rotation += rotation_direction * ROTATION_SPEED * delta
+	handle_input(delta)
 
-    velocity = move_and_slide(velocity)
-    $PlayerTrail.paint_point()
-    
-    # "Animate" the picked up object by slightly rotating it every tick
-    if pickup_object:
-       $PickupSprite.rotation += (2 * delta)
-    
 func _on_InteractionArea_area_entered(area):
-    print("Interactable Object available!")
-    interactable_object = area.get_parent()
+	print("Interactable Object available!")
+	interactable_object = area.get_parent()
 
 func _on_InteractionArea_area_exited(_area):
     print("Interactable Object gone!")
@@ -63,3 +69,4 @@ func drop():
     
     $PickupSprite.texture = null
     $PickupSprite.hide()
+
--- scenes/PlayerTrail.gd
+++ scenes/PlayerTrail.gd
@@ -6,12 +6,31 @@ export(NodePath) var targetPath
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    target = get_node(targetPath)
-    pass # Replace with function body.
+	target = get_node(targetPath)
+	# For sanity
+	clear_points()
+	pass # Replace with function body.
 
 func paint_point():
-    global_position = Vector2(0,0)
-    global_rotation = 0
-    
-    add_point(target.global_position)
-    pass
+	global_position = Vector2(0,0)
+	global_rotation = 0
+
+	var last = get_last()
+	var distance = last.distance_to(target.global_position)
+
+	if last == null || distance > 3:
+		add_point(target.global_position)
+		print(get_point_count())
+	pass
+
+func pop_last():
+	global_position = Vector2(0,0)
+	global_rotation = 0
+
+	var last = get_last()
+	remove_point(get_point_count() -1)
+
+	return last
+
+func get_last():
+	return get_point_position(get_point_count() -1)
