9e988eea4a9f285ccdd8eebe44ce2ab61e29fda4
Marvin Altemeier <marvin.altemeier@hey.com>
2021-09-11 10:48:25 +0200

    Update controls

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,9 +1,27 @@
 extends KinematicBody2D
 
-const SPEED = 500
+const SPEED = 300
+const ROTATION_SPEED = 10;
+
+var velocity = Vector2.ZERO
+var rotation_direction = 0
+
+func handle_input():
+	rotation_direction = 0
+	velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_right"):
+		rotation_direction += 1
+	if Input.is_action_pressed("ui_left"):
+		rotation_direction -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity -= transform.x * SPEED
+	if Input.is_action_pressed("ui_up"):
+		velocity += transform.x * SPEED
 
 func _physics_process(delta):
-    var input_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-    var input_y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-        
-    move_and_slide(Vector2(input_x, input_y) * SPEED)
+	handle_input()
+	rotation += rotation_direction * ROTATION_SPEED * delta
+
+	velocity = move_and_slide(velocity)
+
